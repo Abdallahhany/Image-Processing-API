0628c8cfefc2f73e92c7ce278799f35275798879
Abdallahhany <abdallah.hanaa7706@ci.menofia.edu.eg>
2022-08-17 21:20:46 +0200

    list all thumbed images related to orignal image

--- src/controllers/all_images.ts
+++ src/controllers/all_images.ts
@@ -5,11 +5,19 @@ import path from 'path';
 const allImages = async (req: Request, res: Response): Promise<void> => {
     // display all original images
     const originalDirPath = path.resolve(__dirname, `../../assets/full/`);
-    const images = fs.readdirSync(originalDirPath);
+    const thumbedDirPath = path.resolve(__dirname, `../../assets/thumb/`);
+    const fullImages = fs.readdirSync(originalDirPath);
+    const thumbedImages = fs.readdirSync(thumbedDirPath);
     let data = `<h1> Here is a list of available images (with full size) to use: </h1>`;
-    images.forEach((image, i) => {
+    fullImages.forEach((image, i) => {
         data += `<h3>Image ${i + 1} - ${image}</h3>
-        <img src="../assets/full/${image}" alt="image not found"/>`;
+        <img src="../assets/full/${image}" alt="image not found"/>
+        <h4> Here are thumbed images related to orignal image</h4>`;
+        thumbedImages.forEach((thumb) => {
+            if (thumb.split('_')[0] == image.split('.')[0])
+                data += `
+                    <img src="../assets/thumb/${thumb}" alt="image not found"/>`;
+        });
     });
     res.send(data);
 };
