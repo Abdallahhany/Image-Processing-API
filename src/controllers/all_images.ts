83166aeff8feb226d9c95c2c92a53529b41d3e1b
Abdallahhany <abdallah.hanaa7706@ci.menofia.edu.eg>
2022-08-17 21:05:18 +0200

    fix bug in allimages routes

--- src/controllers/all_images.ts
+++ src/controllers/all_images.ts
@@ -4,14 +4,13 @@ import path from 'path';
 
 const allImages = async (req: Request, res: Response): Promise<void> => {
     // display all original images
-    const originalDirPath = path.resolve(__dirname, `../../../assets/full/`);
+    const originalDirPath = path.resolve(__dirname, `../../assets/full/`);
     const images = fs.readdirSync(originalDirPath);
     let data = `<h1> Here is a list of available images (with full size) to use: </h1>`;
     images.forEach((image, i) => {
         data += `<h3>Image ${i + 1} - ${image}</h3>
         <img src="../assets/full/${image}" alt="image not found"/>`;
     });
-
     res.send(data);
 };
 
